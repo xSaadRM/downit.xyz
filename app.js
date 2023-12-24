78756a94863feab81ac5df4ea421918ce66be535
xSaadRM <saadrm.dev@gmail.com>
2023-12-24 14:32:12 +0100

    mp4 working

--- app.js
+++ app.js
@@ -28,25 +28,66 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-// Endpoint to get video information
+// Endpoint to get video information including available formats
 app.post('/getVideoInfo', async (req, res) => {
-  const { videoUrl } = req.body;
+    const { videoUrl } = req.body;
+  
+    try {
+      const info = await ytdl.getInfo(videoUrl);
+      const videoId = ytdl.getURLVideoID(videoUrl);
+      console.log("THE VID ID: ", videoId)
+      // Extract video details
+      const title = info.videoDetails.title;
+      const author = info.videoDetails.author.name;
+      const description = info.videoDetails.description;
+      const thumbnailURL = info.videoDetails.thumbnails[0].url;
+  
+      // Extract available formats
+      const availableFormats = info.formats.map((format, index) => ({
+        formatId: format.itag,
+        resolution: format.qualityLabel || 'Unknown',
+        videoCodec: format.hasVideo ? format.codecs : 'None',
+        audioCodec: format.hasAudio ? format.audioCodecs : 'None',
+        bitrate: format.bitrate
+      }));
+  
+      // Send video details and available formats as JSON response
+      res.json({
+        title,
+        videoId,
+        author,
+        description,
+        thumbnailURL,
+        videoUrl,
+        availableFormats
+      });
+    } catch (err) {
+      console.error('Error fetching video info:', err);
+      res.status(500).json({ error: 'Error fetching video info' });
+    }
+  });
 
-  try {
-    const info = await ytdl.getBasicInfo(videoUrl);
-
-    // Extract video details
-    const title = info.videoDetails.title;
-    const author = info.videoDetails.author.name;
-    const description = info.videoDetails.description;
-    const thumbnailURL = info.videoDetails.thumbnails[0].url;
-
-    // Send video details as JSON response
-    res.json({ title, author, description, thumbnailURL, videoUrl });
-  } catch (err) {
-    console.error('Error fetching video info:', err);
-    res.status(500).json({ error: 'Error fetching video info' });
-  }
+// Endpoint to download the video based on the format ID
+app.get('/download/:videoId/:formatId', async (req, res) => {
+    const { videoId, formatId } = req.params;
+  
+    try {
+      const videoInfo = await ytdl.getInfo(`https://www.youtube.com/watch?v=${videoId}`);
+      const format = videoInfo.formats.find(format => format.itag === parseInt(formatId));
+  
+      if (format) {
+        const filename = `${videoInfo.videoDetails.title.replace(/[^\w\s]/gi, '')}.${format.container}`;
+        res.header('Content-Disposition', `attachment; filename="${filename}"`);
+        
+        // Using ytdl.downloadFromInfo to download the video
+        const options = { format: format };
+        ytdl.downloadFromInfo(videoInfo, options).pipe(res);
+      } else {
+        res.status(404).send('Format not found');
+      }
+    } catch (error) {
+      res.status(500).send('Error downloading the video');
+    }
 });
 
 const PORT = process.env.PORT || 3000;
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,12 +6,14 @@ document.addEventListener('DOMContentLoaded', function() {
     videoForm.addEventListener('submit', async function(event) {
       event.preventDefault();
       const videoURL = document.getElementById('videoURL').value;
-  
       // Show loading animation
       loadingAnimation.style.display = 'block';
       videoInfoDiv.innerHTML = ''; // Clear any existing content
   
       try {
+        
+        
+  
         const response = await fetch(`/getVideoInfo`, {
           method: 'POST',
           headers: {
@@ -21,7 +23,8 @@ document.addEventListener('DOMContentLoaded', function() {
         });
   
         const data = await response.json();
-        displayVideoInfo(data);
+        const videoId = data.videoId;
+        displayVideoInfo(data, videoId); // Pass videoId to display function
       } catch (error) {
         videoInfoDiv.textContent = `Error: ${error.message}`;
       } finally {
@@ -29,48 +32,70 @@ document.addEventListener('DOMContentLoaded', function() {
         loadingAnimation.style.display = 'none';
       }
     });
-  
     function displayVideoInfo(data) {
-        // Display video details in the HTML
-        const videoDetailsContainer = document.getElementById('videoInfo');
-        videoDetailsContainer.innerHTML = `
-          <div style="text-align: center;">
-            <h2>Title: ${data.title}</h2>
-            <p>Author: ${data.author}</p>
-          </div>
-          <div class="thumbnail-container">
-            <img src="${data.thumbnailURL}" alt="Thumbnail">
-          </div>
-          <div class="description-container">
-            <p>Description:</p>
-            <div class="description-scroll">${data.description}</div>
-          </div>
-          <div class="download-container">
-            <h3>Available Resolutions:</h3>
-            <ul id="downloadList"></ul>
-          </div>
-        `;
-      
-        const downloadList = document.getElementById('downloadList');
-      
-        // Check if data.formats exists and is an array
-        if (Array.isArray(data.formats) && data.formats.length > 0) {
-          data.formats.forEach((format, index) => {
-            const listItem = document.createElement('li');
-            const downloadButton = document.createElement('button');
-      
-            downloadButton.textContent = `Download ${format.quality}`;
-            downloadButton.addEventListener('click', () => {
-              downloadVideo(format.url, data.title, index);
-            });
-      
-            listItem.appendChild(downloadButton);
-            downloadList.appendChild(listItem);
-          });
-        } else {
-          const noFormatsMessage = document.createElement('p');
-          noFormatsMessage.textContent = 'No available formats to download.';
-          downloadList.appendChild(noFormatsMessage);
-        }
+      const videoDetailsContainer = document.getElementById('videoInfo');
+      videoDetailsContainer.innerHTML = `
+        <div style="text-align: center;">
+          <h2>Title: ${data.title}</h2>
+          <p>Author: ${data.author}</p>
+        </div>
+        <div class="thumbnail-container">
+          <img src="${data.thumbnailURL}" alt="Thumbnail">
+        </div>
+        <div class="description-container">
+          <p>Description:</p>
+          <div class="description-scroll">${data.description}</div>
+        </div>
+        <div class="download-container">
+          <h3>Available Resolutions:</h3>
+          <ul id="downloadList"></ul>
+        </div>
+      `;
+  
+      const downloadList = document.getElementById('downloadList');
+
+      if (Array.isArray(data.availableFormats) && data.availableFormats.length > 0) {
+        data.availableFormats.forEach((format, index) => {
+          const listItem = document.createElement('li');
+          const downloadButton = document.createElement('button');
+  
+          downloadButton.textContent = `Download ${format.resolution}`;
+          downloadButton.addEventListener('click', async (event) => {
+            const videoId = data.videoId;
+              await initiateVideoDownload(format.formatId, videoId);
+            }),
+          listItem.appendChild(downloadButton);
+          downloadList.appendChild(listItem);
+        });
+      } else {
+        const noFormatsMessage = document.createElement('p');
+        noFormatsMessage.textContent = 'No available formats to download.';
+        downloadList.appendChild(noFormatsMessage);
       }
-    });
+    }
+  
+    async function initiateVideoDownload(formatId, videoId) {
+      try {
+        const response = await fetch(`/download/${videoId}/${formatId}`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok.');
+        }
+        const blob = await response.blob();
+        downloadBlob(blob, `${videoId}.mp4`);
+      } catch (error) {
+        console.error('Error downloading the video:', error);
+        // Handle errors or display an error message to the user
+      }
+    }
+
+    function downloadBlob(blob, filename) {
+      const blobURL = URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = blobURL;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    }
+  
+  });
