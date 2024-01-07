502495fcbf651bc93bb2555fcbf9fb0dff6e2373
xSaadRM <saadrm.dev@gmail.com>
2024-01-07 23:53:37 +0100

    fixed the shifting

--- public/scripts/topmenu.js
+++ public/scripts/topmenu.js
@@ -1,8 +1,9 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const topMenu = document.querySelector('.top-menu ul');
 const pageContent = document.querySelector('.container'); // Replace with your content container class
-
+const darkIcon = document.getElementById('darkModeToggle')
 menuToggle.addEventListener('click', () => {
     topMenu.classList.toggle('active');
     pageContent.classList.toggle('content-shifted');
+    darkIcon.classList.toggle('content-shifted')
 });
